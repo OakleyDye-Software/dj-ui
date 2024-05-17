85614a87b7367e5c8fa98ebf41d138a348be1f61
Oakley Dye <oakley.dye@gmail.com>
2024-05-17 00:26:01 -0600

    formatting

--- src/components/common/menu-bar.tsx
+++ src/components/common/menu-bar.tsx
@@ -20,12 +20,31 @@ const MenuBar: React.FC<MenuBarProps> = ({ className, currentSection }) => {
 
     return (  
         <React.Fragment>
-            <div className={`menu-bar ${className}`} style={{ color }}>
-                <Box sx={{ zIndex: 1, position: 'absolute', top: '1rem', left: '1rem'}}>
+            <div 
+                className={`menu-bar ${className}`} 
+                style={{ color }}
+            >
+                <Box 
+                    sx={{ 
+                        zIndex: 1, 
+                        position: 'absolute', 
+                        top: '1rem', 
+                        left: '1rem'
+                    }}
+                >
                     {/* <img src={'test'} alt="DJ OXLEY" width={100} height={100} /> */}
                     <Typography variant="h4" component="h4" sx={{ ml: 1 }}>CD ENTERTAINMENT</Typography>
                 </Box>
-                <Box sx={{ zIndex: 1, position: 'absolute', top: '1rem', right: '1rem', display: 'flex', alignItems: 'center' }}>
+                <Box 
+                    sx={{ 
+                        zIndex: 1, 
+                        position: 'absolute', 
+                        top: '1rem', 
+                        right: '1rem', 
+                        display: 'flex', 
+                        alignItems: 'center' 
+                    }}
+                >
                     <MenuIcon sx={{ fontSize: 45 }}/>
                     <Typography variant="h4" component="h4" sx={{ ml: 1 }}>Menu</Typography>
                 </Box>
