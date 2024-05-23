398648f13a03b44303927e849c72322bd51b6256
Oakley Dye <oakley.dye@gmail.com>
2024-05-22 21:50:37 -0600

    added header to contact section

--- src/components/homepage/contact-section.tsx
+++ src/components/homepage/contact-section.tsx
@@ -1,11 +1,15 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import * as React from 'react';
 import ContactForm from '../common/contact-form';
+import { useDJContext } from '../../logic/state/GlobalContext';
 
 const ContactSection: React.FC = () => {
+    const { isMobile } = useDJContext();
+
     return (  
         <React.Fragment>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+                <Typography variant={isMobile ? 'h4' : 'h2'} component="h2" sx={{ textAlign: 'center', pt: 10, pb: 5 }}>Contact Us</Typography>
                 <ContactForm />
             </Box>
         </React.Fragment>
