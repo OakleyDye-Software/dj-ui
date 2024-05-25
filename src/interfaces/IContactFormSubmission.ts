e7ad86f482a3314926a659672d21323f00b99a9d
Oakley Dye <oakley.dye@gmail.com>
2024-05-25 00:47:20 -0600

    added interface

---
+++ src/interfaces/IContactFormSubmission.ts
@@ -0,0 +1,10 @@
+export interface IContactFormSubmission {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber: string;
+    eventTypeId: number;
+    venueLocation: string;
+    dateOfEvent: string;
+    eventDescription: string;
+}
