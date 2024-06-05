e10aa2791a3da6d25ea3c50fd4e27b03a0a9551e
Oakley Dye <oakley.dye@gmail.com>
2024-06-05 14:15:33 -0600

    footer styling

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -17,38 +17,38 @@ const Footer: React.FC = () => {
                         <Box sx={{ display: 'flex', flexDirection: 'column' }} className='footer-section'>
                             <Typography variant='h6'>Company</Typography>
                             <ul>
-                                <Link href={`/about`}><li>About Us</li></Link>
-                                <Link href={`/services`}><li>Services</li></Link>
-                                <Link href={`/portfolio`}><li>Portfolio</li></Link>
-                                <Link href={`/testimonials`}><li>Testimonials</li></Link>
-                                <Link href={`/contact`}><li>Contact Us</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/about`}><li>About Us</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/services`}><li>Services</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/portfolio`}><li>Portfolio</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/testimonials`}><li>Testimonials</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/contact`}><li>Contact Us</li></Link>
                             </ul>
                         </Box>
                         <Box sx={{ display: 'flex', flexDirection: 'column' }} className='footer-section'>
                             <Typography variant='h6'>Services</Typography>
                             <ul>
-                                <Link href={`/services/weddings`}><li>Weddings</li></Link>
-                                <Link href={`/services/dances`}><li>Dances</li></Link>
-                                <Link href={`/services/parties`}><li>Parties</li></Link>
-                                <Link href={`/services/corporate-events`}><li>Corporate Events</li></Link>
-                                <Link href={`/services/live-sound`}><li>Live Sound</li></Link>
-                                <Link href={`/services/gear-rental`}><li>Gear Rental</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/services/weddings`}><li>Weddings</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/services/dances`}><li>Dances</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/services/parties`}><li>Parties</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/services/corporate-events`}><li>Corporate Events</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/services/live-sound`}><li>Live Sound</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/services/gear-rental`}><li>Gear Rental</li></Link>
                             </ul>
                         </Box>
                         <Box sx={{ display: 'flex', flexDirection: 'column' }} className='footer-section'>
                             <Typography variant='h6'>Legal</Typography>
                             <ul>
-                                <Link href={`/legal/privacy-policy`}><li>Privacy Policy</li></Link>
-                                <Link href={`/legal/terms-and-conditions`}><li>Terms & Conditions</li></Link>
-                                <Link href={`/legal/cookie-policy`}><li>Cookie Policy</li></Link>
-                                <Link href={`/legal/disclaimer`}><li>Disclaimer</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/legal/privacy-policy`}><li>Privacy Policy</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/legal/terms-and-conditions`}><li>Terms & Conditions</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/legal/cookie-policy`}><li>Cookie Policy</li></Link>
+                                <Link sx={{ color: 'white' }} href={`/legal/disclaimer`}><li>Disclaimer</li></Link>
                             </ul>
                         </Box>
                         <Box sx={{ display: 'flex', flexDirection: 'column' }} className='footer-section'>
                             <Typography variant='h6'>Contact Us</Typography>
                             <ul>
-                                <Link href={'tel:+14355571427'}><li>435-557-1427</li></Link>
-                                <Link href={'mailto:dj@oakleydye.com'}><li>dj@oakleydye.com</li></Link>
+                                <Link sx={{ color: 'white' }} href={'tel:+14355571427'}><li>435-557-1427</li></Link>
+                                <Link sx={{ color: 'white' }} href={'mailto:dj@oakleydye.com'}><li>dj@oakleydye.com</li></Link>
                                 <li>Hyrum, UT</li>
                             </ul>
                         </Box>
