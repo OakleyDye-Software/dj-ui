15caf2ccf956c8ef1190373cfe97f6f046c3515f
Oakley Dye <oakley.dye@gmail.com>
2024-06-17 23:46:39 -0600

    feat: Add testimonials section with placeholder text

--- src/components/testimonials/testimonials.tsx
+++ src/components/testimonials/testimonials.tsx
@@ -1,9 +1,12 @@
+import { Typography } from '@mui/material';
 import * as React from 'react';
 
 const Testimonials: React.FC = () => {
     return (  
         <React.Fragment>
-            
+            <Typography variant="h3" gutterBottom>
+                Testimonials Coming Soon!
+            </Typography>
         </React.Fragment>
     );
 }
