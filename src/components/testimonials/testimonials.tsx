e54c18708e247a7c7a29da689e7b7dbf9314de56
Oakley Dye <oakley.dye@gmail.com>
2024-06-18 08:54:37 -0600

    refactor: Update testimonials page layout and add common components

--- src/components/testimonials/testimonials.tsx
+++ src/components/testimonials/testimonials.tsx
@@ -1,12 +1,18 @@
-import { Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import * as React from 'react';
+import MenuBar from '../common/menu-bar';
+import Footer from '../common/footer';
 
 const Testimonials: React.FC = () => {
     return (  
         <React.Fragment>
-            <Typography variant="h3" gutterBottom>
-                Testimonials Coming Soon!
-            </Typography>
+            <MenuBar className='fixedMenuBar' currentSection='default' />
+            <Container maxWidth="lg" sx={{ mt: 15 }}>
+                <Typography variant="h3" gutterBottom>
+                    Testimonials Coming Soon!
+                </Typography>
+            </Container>
+            <Footer />
         </React.Fragment>
     );
 }
